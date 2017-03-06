fb6e02ed45484955ab9bcc4830098d1c566326ee
大原幸夫 <roripika@gmail.com>
2017-03-06 19:14:01 +0900

    アンドロイドビルドが可能にする

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -11,11 +11,26 @@ LOCAL_MODULE := MyGame_shared
 
 LOCAL_MODULE_FILENAME := libMyGame
 
+LOCAL_SRC_FILES_JNI_PREFIXED := \
+    $(wildcard $(LOCAL_PATH)/../../Classes/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/../../Classes/**/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/../../Classes/**/**/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/../../Classes/**/**/**/*.cpp)
+
+LOCAL_SRC_FILES_JNI_UNPREFIXED := $(subst jni/,, $(LOCAL_SRC_FILES_JNI_PREFIXED))
+
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   $(LOCAL_SRC_FILES_JNI_UNPREFIXED)
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+$(info $(LOCAL_SRC_FILES))
+
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
+					$(LOCAL_PATH)/../../Classes/Manager \
+					$(LOCAL_PATH)/../../Classes/Object \
+					$(LOCAL_PATH)/../../Classes/puzzle \
+					$(LOCAL_PATH)/../../Classes/Scene \
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
